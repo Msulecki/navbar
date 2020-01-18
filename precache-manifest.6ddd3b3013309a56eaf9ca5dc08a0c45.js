d787a00f7c3588667ed38ea6b4700e40403c26f7
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-01-18 23:04:53 +0100

    Updates

---
+++ precache-manifest.6ddd3b3013309a56eaf9ca5dc08a0c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a782b660e0324e4203e213d525ef4510",
+    "url": "/navbar/index.html"
+  },
+  {
+    "revision": "f252a33fb9eb261a8e88",
+    "url": "/navbar/static/css/main.e79c9c2d.chunk.css"
+  },
+  {
+    "revision": "dcf889c0db7e2c3f3034",
+    "url": "/navbar/static/js/2.2a3978e4.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/navbar/static/js/2.2a3978e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f252a33fb9eb261a8e88",
+    "url": "/navbar/static/js/main.8ada9ef5.chunk.js"
+  },
+  {
+    "revision": "a4069f17bd3d2dea164b",
+    "url": "/navbar/static/js/runtime-main.45e7836c.js"
+  }
+]);
